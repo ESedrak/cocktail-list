44c24167a16abb3cee9b4d3774b0285499d0756f
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-21 12:34:08 +1000

    Linked up css from original project

--- src/Cocktail.js
+++ src/Cocktail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Cocktail.css";
 
 function Cocktail() {
   return (
@@ -11,24 +12,24 @@ function Cocktail() {
       </p>
       <hr />
       <div>
-        <div class="image">
+        <div className="image">
           <img
-            class="image__drink"
+            className="image__drink"
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/023/000/original/french-75-cocktail.jpg?1639738210"
             alt="French75"
           />
-          <div class="image__overlay">
-            <div class="image__title">
+          <div className="image__overlay">
+            <div className="image__title">
               <h2>French 75</h2>
             </div>
-            <ul class="image__description">
+            <ul className="image__description">
               <li>45mls of Gin or Brandy</li>
               <li>30mls of Cointreau</li>
               <li>30mls of Freshly Squeezed Lime Juice</li>
               <li>15mls of Simple Syrup</li>
             </ul>
             <p>
-              <span class="process">
+              <span className="process">
                 Shake ingredients with ice, strain into a champagne glass and
                 top up with Prosecco
               </span>
@@ -36,29 +37,29 @@ function Cocktail() {
           </div>
         </div>
 
-        <div class="image">
+        <div className="image">
           <img
-            class="image__drink"
+            className="image__drink"
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/022/999/original/Whiskey_Sour_Served_Up.jpg?1639734669"
             alt="Sour"
           />
-          <div class="image__overlay">
-            <div class="image__title">
+          <div className="image__overlay">
+            <div className="image__title">
               <h2>Sours</h2>
             </div>
-            <ul class="image__description">
+            <ul className="image__description">
               <li>60mls of Whisky or Amaretto</li>
               <li>30mls of Freshly Squeezed Lemon Juice</li>
               <li>1 Egg White</li>
               <li>15mls of Simple Syrup</li>
             </ul>
             <p>
-              <span class="process">
+              <span className="process">
                 Shake ingredients with ice and pour into a whisky glass
               </span>
             </p>
             <p>
-              <span class="optional_topping">
+              <span className="optional_topping">
                 <strong> Optional: </strong>
                 <br />
                 <em>If using Whisky:</em>
@@ -74,29 +75,29 @@ function Cocktail() {
           </div>
         </div>
 
-        <div class="image">
+        <div className="image">
           <img
-            class="image__drink"
+            className="image__drink"
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/023/002/original/Tommys_Margarita_Background_2x.png?1639738631"
             alt="Tommy"
           />
-          <div class="image__overlay">
-            <div class="image__title">
+          <div className="image__overlay">
+            <div className="image__title">
               <h2>Tommy's Margarita</h2>
             </div>
-            <ul class="image__description">
+            <ul className="image__description">
               <li>90mls of Tequila</li>
               <li>40mls of Freshly Squeezed Lime Juice</li>
               <li>15mls of Agave Syrup or 15mls Cointreau</li>
             </ul>
             <p>
-              <span class="process">
+              <span className="process">
                 Shake ingredients with ice and pour into a glass rimmed with
                 salt
               </span>
             </p>
             <p>
-              <span class="optional_topping">
+              <span className="optional_topping">
                 <strong> Optional: </strong>
                 <br />
                 Blend with ice to make a frozen Margarita
