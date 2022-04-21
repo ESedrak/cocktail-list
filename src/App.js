4f011fb0a4f1555575abc750fa6542f52b1a93e9
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-21 12:30:49 +1000

    Transferred over original information from first project(still need to clean it up)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Cocktail from "Cocktail";
+import Cocktail from "./Cocktail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <Cocktail />
+      <header className="App-header">
+        <Cocktail />
+      </header>
     </div>
   );
 }
