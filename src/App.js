f842c26a85f6cd7f6abe2d7f3769da760aee43a5
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-09 08:05:19 +1000

    Adding React Router to allow for 'More cocktails'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
 import Cocktail from "./Cocktail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const More = () => {
+    return <h2>More</h2>;
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bar of Liz</h1>
         <p>The perfected ratios of some of the best cocktails</p>
+        <Router>
+          <Link to="/more">More</Link>
+        </Router>
         <hr />
       </header>
       <Cocktail />
+      <Switch>
+        <Route path="/more">
+          <More />
+        </Route>
+      </Switch>
     </div>
   );
 }
