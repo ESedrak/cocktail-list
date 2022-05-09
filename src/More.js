bb4c3b7e24f2d1a045af675064f588849c8ac5e6
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-09 12:44:55 +1000

    Started to link the search bar to the url

--- src/More.js
+++ src/More.js
@@ -1,14 +1,29 @@
+import React, { useState } from "react";
+
 const More = () => {
+  const [keyword, setKeyword] = useState(null);
+
   const handleSearch = (e) => {
     e.preventDefault();
-    const urlCocktail = `www.thecocktaildb.com/api/json/v1/1/search.php?s`;
+    // Receiving information from the search bar into the URL below
+    const urlCocktail = `www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword}`;
+    console.log(urlCocktail);
+  };
+
+  const cocktailSearch = (e) => {
+    setKeyword(e.target.value);
+    //Value of the search bar
   };
 
   return (
     <div className="More">
-      <h2>More(... in progress)</h2>
+      <h2>More(... in progress) - Special thanks to TheCocktailDB</h2>
       <form onSubmit={handleSearch}>
-        <input type="search" placeholder="Search for a drink"></input>
+        <input
+          type="search"
+          placeholder="Search for a drink"
+          onChange={cocktailSearch}
+        ></input>
         <button type="button" onClick={handleSearch}>
           🔍
         </button>
