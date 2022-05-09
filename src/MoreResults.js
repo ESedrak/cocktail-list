92a5745cecb936fd10b11b9f826f8317a3d0cb8e
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-10 09:50:50 +1000

    Mapped through ingredients and measurements

--- src/MoreResults.js
+++ src/MoreResults.js
@@ -1,19 +1,38 @@
 import React from "react";
 
-function MoreResults(props) {
+const MoreResults = (props) => {
   //  console.log(props.results) to see data passed through from component More
   console.log(props.results);
   if (props.results) {
     return (
-      <div className="Results">
-        {props.results.map((name, index) => {
-          return <h2 key={index}>{name.strDrink}</h2>;
+      <div className="MoreResults">
+        {props.results.map((cocktail, index) => {
+          return (
+            <div key={index}>
+              <h2>{cocktail.strDrink}</h2>
+              <img
+                src={cocktail.strDrinkThumb}
+                alt={cocktail.idDrink}
+                width="100px"
+              ></img>
+              <p>{cocktail.strIngredient1}</p>
+              <p>{cocktail.strMeasure1}</p>
+              <p>{cocktail.strIngredient2}</p>
+              <p>{cocktail.strMeasure2}</p>
+              <p>{cocktail.strIngredient3}</p>
+              <p>{cocktail.strMeasure3}</p>
+              <p>{cocktail.strIngredient4}</p>
+              <p>{cocktail.strMeasure4}</p>
+              <p>{cocktail.strIngredient5}</p>
+              <p>{cocktail.strMeasure5}</p>
+            </div>
+          );
         })}
       </div>
     );
   } else {
     return null;
   }
-}
+};
 
 export default MoreResults;
