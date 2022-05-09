6f7f2092bcf5e85f0c1a756dd7c82c2a2574933a
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-10 08:46:19 +1000

    Added blue genie photo ? may be temp(want to take a better photo)

--- src/Genie.js
+++ src/Genie.js
@@ -1,14 +1,11 @@
 import React from "react";
+import BlueGenie from "./images/BlueGenie.jpg";
 
 function Genie() {
   return (
     <div className="Genie">
       <section className="one-cocktail">
-        <img
-          className="image-drink"
-          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/023/000/original/french-75-cocktail.jpg?1639738210"
-          alt="French75"
-        />
+        <img className="image-drink" src={BlueGenie} alt="French75" />
         <div className="image-overlay">
           <h2 className="drink-name">Blue Genie</h2>
           <ul className="image-description">
--- src/MoreResults.js
+++ src/MoreResults.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function MoreResults(props) {
   //  console.log(props.results) to see data passed through from component More
+  console.log(props.results);
   return <div className="Results"> Results</div>;
 }
 
