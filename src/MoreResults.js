d60f6dffcf1e2ca3b23758984009eed83dce2cc8
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-10 11:32:50 +1000

    Added Rows/Columns to ingredients/measurements

--- src/MoreResults.js
+++ src/MoreResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const MoreResults = (props) => {
   //  console.log(props.results) to see data passed through from component More
@@ -6,29 +7,53 @@ const MoreResults = (props) => {
   if (props.results) {
     return (
       <div className="MoreResults">
-        {props.results.map((cocktail, idDrink) => {
-          return (
-            <div key={idDrink}>
-              <h2>{cocktail.strDrink}</h2>
-              <img
-                src={cocktail.strDrinkThumb}
-                alt={cocktail.strCategory}
-                width="100px"
-              ></img>
-              <p>{cocktail.strIngredient1}</p>
-              <p>{cocktail.strMeasure1}</p>
-              <p>{cocktail.strIngredient2}</p>
-              <p>{cocktail.strMeasure2}</p>
-              <p>{cocktail.strIngredient3}</p>
-              <p>{cocktail.strMeasure3}</p>
-              <p>{cocktail.strIngredient4}</p>
-              <p>{cocktail.strMeasure4}</p>
-              <p>{cocktail.strIngredient5}</p>
-              <p>{cocktail.strMeasure5}</p>
-              <p>{cocktail.strInstructions}</p>
-            </div>
-          );
-        })}
+        <Container>
+          {props.results.map((cocktail, idDrink) => {
+            return (
+              <div key={idDrink}>
+                <h2>{cocktail.strDrink}</h2>
+                <img
+                  src={cocktail.strDrinkThumb}
+                  alt={cocktail.strCategory}
+                  width="100px"
+                ></img>
+                <Row>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient1} {cocktail.strMeasure1}
+                    </span>
+                  </Col>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient2} {cocktail.strMeasure2}
+                    </span>
+                  </Col>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient3} {cocktail.strMeasure3}
+                    </span>
+                  </Col>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient4} {cocktail.strMeasure4}
+                    </span>
+                  </Col>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient5} {cocktail.strMeasure5}
+                    </span>
+                  </Col>
+                  <Col sm={4}>
+                    <span>
+                      {cocktail.strIngredient6} {cocktail.strMeasure6}
+                    </span>
+                  </Col>
+                </Row>
+                <p>{cocktail.strInstructions}</p>
+              </div>
+            );
+          })}
+        </Container>
       </div>
     );
   } else {
