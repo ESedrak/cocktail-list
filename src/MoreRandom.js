9e91b3a7b507a32894378c43a2d65a403e32704c
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-13 15:02:07 +1000

    Start of the random cocktail page

--- src/MoreRandom.js
+++ src/MoreRandom.js
@@ -3,13 +3,21 @@ import React from "react";
 const MoreRandom = () => {
   const randomCocktail = (e) => {
     e.preventDefault();
+
+    const randomUrl = "www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    fetch(randomUrl)
+      .then((response) => response.json())
+      .then((data) => data.drinks);
   };
 
   return (
     <div className="MoreRandom">
-      <button className="MoreRandom Button" onChange={randomCocktail}>
-        Random Cocktail
-      </button>
+      <form onChange={randomCocktail}>
+        <button className="MoreRandom Button" onChange={randomCocktail}>
+          Random Cocktail! ...In progress
+        </button>
+      </form>
     </div>
   );
 };
