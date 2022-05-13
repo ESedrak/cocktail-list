8d7a69259b1acb9736be093e75ee868dfe982a12
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-13 16:58:23 +1000

    Receiving a response from randomCocktail

--- src/MoreRandom.js
+++ src/MoreRandom.js
@@ -1,20 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 const MoreRandom = () => {
+  const handleRandomSearch = (response) => {
+    console.log(response.data.drinks[0]);
+  };
+
   const randomCocktail = (e) => {
     e.preventDefault();
 
-    const randomUrl = "www.thecocktaildb.com/api/json/v1/1/random.php";
+    const randomUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-    fetch(randomUrl)
-      .then((response) => response.json())
-      .then((data) => data.drinks);
+    axios.get(randomUrl).then(handleRandomSearch);
   };
 
   return (
     <div className="MoreRandom">
       <form onChange={randomCocktail}>
-        <button className="MoreRandom Button" onChange={randomCocktail}>
+        <button className="MoreRandom Button" onClick={randomCocktail}>
           Random Cocktail! ...In progress
         </button>
       </form>
