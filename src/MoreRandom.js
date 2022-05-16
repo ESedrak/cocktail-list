5f9f73fe18f8e834810557617bf8ab5fd9b7ea2a
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-16 10:07:42 +1000

    Used fetch instead of axios, created a new component MoreRandomData that is receiving the array of data from MoreRandom

--- src/MoreRandom.js
+++ src/MoreRandom.js
@@ -1,9 +1,37 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import MoreRandomData from "./MoreRandomData";
 
 const MoreRandom = () => {
-  const handleRandomSearch = (response) => {
-    console.log(response.data.drinks[0]);
+  const [randomData, setRandomData] = useState(null);
+
+  const handleRandomResponse = (response) => {
+    setRandomData({
+      name: response.drinks[0].strDrink,
+      tags: response.drinks[0].strTags,
+      category: response.drinks[0].strCategory,
+      image: response.drinks[0].strImageSource,
+      id: response.drinks[0].idDrink,
+      instructions: response.drinks[0].strInstructions,
+      ingredient1: response.drinks[0].strIngredient1,
+      ingredient2: response.drinks[0].strIngredient2,
+      ingredient3: response.drinks[0].strIngredient3,
+      ingredient4: response.drinks[0].strIngredient4,
+      ingredient5: response.drinks[0].strIngredient5,
+      ingredient6: response.drinks[0].strIngredient6,
+      ingredient7: response.drinks[0].strIngredient7,
+      ingredient8: response.drinks[0].strIngredient8,
+      ingredient9: response.drinks[0].strIngredient9,
+      measure1: response.drinks[0].strMeasure1,
+      measure2: response.drinks[0].strMeasure2,
+      measure3: response.drinks[0].strMeasure3,
+      measure4: response.drinks[0].strMeasure4,
+      measure5: response.drinks[0].strMeasure5,
+      measure6: response.drinks[0].strMeasure6,
+      measure7: response.drinks[0].strMeasure7,
+      measure8: response.drinks[0].strMeasure8,
+      measure9: response.drinks[0].strMeasure9,
+    });
+    console.log(response.drinks);
   };
 
   const randomCocktail = (e) => {
@@ -11,7 +39,10 @@ const MoreRandom = () => {
 
     const randomUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-    axios.get(randomUrl).then(handleRandomSearch);
+    // axios.get(randomUrl).then(handleRandomSearch);
+    fetch(randomUrl)
+      .then((response) => response.json())
+      .then(handleRandomResponse);
   };
 
   return (
@@ -20,6 +51,7 @@ const MoreRandom = () => {
         <button className="MoreRandom Button" onClick={randomCocktail}>
           Random Cocktail! ...In progress
         </button>
+        <MoreRandomData randomData={randomData} />
       </form>
     </div>
   );
---
+++ src/MoreRandomData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MoreRandomData = (props) => {
+  return <h1>Random Data</h1>;
+};
+
+export default MoreRandomData;
