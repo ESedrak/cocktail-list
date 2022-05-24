d92e1634600589d733c743b0852b6bd0f78dcd4b
ESedrak <elizabeth_sedrak@hotmail.com>
2022-05-24 10:52:32 +1000

    Removed containers/row/col from MoreCocktails (put them in divs so items are listed one below each other)

--- src/MoreResults.js
+++ src/MoreResults.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import "./MoreResults.css";
 import "./Cocktail.css";
 
@@ -8,60 +7,58 @@ const MoreResults = (props) => {
   if (props.results) {
     return (
       <div className="MoreResults">
-        <Container>
-          {props.results.map((cocktail, index) => {
-            return (
-              <section key={index} className="Cocktail-one">
-                <img
-                  src={cocktail.strDrinkThumb}
-                  alt={cocktail.strCategory}
-                  width="100px"
-                  className="Cocktail-image-drink"
-                ></img>
-                <div className="Cocktail-image-overlay">
-                  <h2 className="Cocktail-drink-name">{cocktail.strDrink}</h2>
-                  {/* The API list has room for up to 15 ingredients/measurements - however, haven't seen more than 8 (have allowed room for 9).
+        {props.results.map((cocktail, index) => {
+          return (
+            <section key={index} className="Cocktail-one">
+              <img
+                src={cocktail.strDrinkThumb}
+                alt={cocktail.strCategory}
+                width="100px"
+                className="Cocktail-image-drink"
+              ></img>
+              <div className="Cocktail-image-overlay">
+                <h2 className="Cocktail-drink-name">{cocktail.strDrink}</h2>
+                {/* The API list has room for up to 15 ingredients/measurements - however, haven't seen more than 8 (have allowed room for 9).
                  if needed, can add more (but at the time it is unnecessary) */}
-                  <Row className="MoreResults-ingredients">
-                    <Col sm={4}>
-                      {cocktail.strIngredient1} {cocktail.strMeasure1}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient2} {cocktail.strMeasure2}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient2} {cocktail.strMeasure2}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient3} {cocktail.strMeasure3}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient4} {cocktail.strMeasure4}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient5} {cocktail.strMeasure5}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient6} {cocktail.strMeasure6}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient7} {cocktail.strMeasure7}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient8} {cocktail.strMeasure8}
-                    </Col>
-                    <Col sm={4}>
-                      {cocktail.strIngredient9} {cocktail.strMeasure9}
-                    </Col>
-                  </Row>
-                  <p className="MoreResults-instructions">
-                    {cocktail.strInstructions}
-                  </p>
+                <div className="MoreResults-ingredients">
+                  <div>
+                    {cocktail.strIngredient1} {cocktail.strMeasure1}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient2} {cocktail.strMeasure2}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient2} {cocktail.strMeasure2}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient3} {cocktail.strMeasure3}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient4} {cocktail.strMeasure4}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient5} {cocktail.strMeasure5}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient6} {cocktail.strMeasure6}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient7} {cocktail.strMeasure7}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient8} {cocktail.strMeasure8}
+                  </div>
+                  <div>
+                    {cocktail.strIngredient9} {cocktail.strMeasure9}
+                  </div>
                 </div>
-              </section>
-            );
-          })}
-        </Container>
+                <p className="MoreResults-instructions">
+                  {cocktail.strInstructions}
+                </p>
+              </div>
+            </section>
+          );
+        })}
       </div>
     );
   } else {
