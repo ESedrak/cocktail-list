6944b2e21380251b4a164ad5bb9176a82dd37601
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-03 20:16:09 +1000

    remove unused comments code for random component

--- src/barOfRandom/Random.js
+++ src/barOfRandom/Random.js
@@ -5,66 +5,12 @@ import useRandomApi from "../hooks/useRandomApi";
 
 const Random = () => {
   const { randomData, fetchRandomApi } = useRandomApi();
-  // const [randomData, setRandomData] = useState({}); /*Require {}*/
-  // const [ready, setReady] = useState(false); /* Keep track of first page */
-
-  // const handleRandomResponse = (response) => {
-  //   const drinks = response.drinks[0];
-  //   setRandomData({
-  //     name: drinks.strDrink,
-  //     tags: drinks.strTags,
-  //     category: drinks.strCategory,
-  //     image: drinks.strDrinkThumb,
-  //     id: drinks.idDrink,
-  //     instructions: drinks.strInstructions,
-  //     ingredient1: drinks.strIngredient1,
-  //     ingredient2: drinks.strIngredient2,
-  //     ingredient3: drinks.strIngredient3,
-  //     ingredient4: drinks.strIngredient4,
-  //     ingredient5: drinks.strIngredient5,
-  //     ingredient6: drinks.strIngredient6,
-  //     ingredient7: drinks.strIngredient7,
-  //     ingredient8: drinks.strIngredient8,
-  //     ingredient9: drinks.strIngredient9,
-  //     ingredient10: drinks.strIngredient10,
-  //     ingredient11: drinks.strIngredient11,
-  //     measure1: drinks.strMeasure1,
-  //     measure2: drinks.strMeasure2,
-  //     measure3: drinks.strMeasure3,
-  //     measure4: drinks.strMeasure4,
-  //     measure5: drinks.strMeasure5,
-  //     measure6: drinks.strMeasure6,
-  //     measure7: drinks.strMeasure7,
-  //     measure8: drinks.strMeasure8,
-  //     measure9: drinks.strMeasure9,
-  //     measure10: drinks.strMeasure10,
-  //     measure11: drinks.strMeasure11,
-  //     measure12: drinks.strMeasure12,
-  //   });
-  // };
-
-  // const randomCocktail = () => {
-  //   // URL below generates a random cocktail from the api list of  cocktaildb (over 650 cocktails)
-  //   const randomUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-  //   // Use fetch instead of axios - as issues arose when axios and netlify (hosting platfor) are being used together
-  //   fetch(randomUrl)
-  //     .then((response) => response.json())
-  //     .then(handleRandomResponse);
-  // };
 
   const randomCocktail = (e) => {
     e.preventDefault();
-
     fetchRandomApi();
   };
 
-  // const firstLoad = () => {
-  //   setReady(true);
-  //   randomCocktail();
-  // };
-
-  // if (ready) {
   return (
     <div className="Random">
       <h2 className="Random-heading">Unsure?</h2>
@@ -74,10 +20,6 @@ const Random = () => {
       </form>
     </div>
   );
-  // } else {
-  //   firstLoad();
-  //   return "Loading Random Cockail";
-  // }
 };
 
 export default Random;
