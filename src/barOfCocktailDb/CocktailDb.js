139bfc6d165e8cbab15576468fbbabb254157081
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-03 19:21:00 +1000

    global hook working for cocktailDb

--- src/barOfCocktailDb/CocktailDb.js
+++ src/barOfCocktailDb/CocktailDb.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import CocktailDbResults from "./CocktailDbResults";
 import "./CocktailDb.css";
 import searchIcon from "../images/search.jpg";
-import useCocktailApi from "./hooks/useCocktailApi";
+import useCocktailApi from "../hooks/useCocktailApi";
 
 const CocktailDb = (props) => {
   // const [keyword, setKeyword] = useState(props.defaultDrink); // keyword is set to default  "Margarita"
@@ -26,32 +26,27 @@ const CocktailDb = (props) => {
   //   fetchCocktailApi();
   // };
 
-  // if (loaded) {
-  //   return (
-  //     <div className="CocktailDb">
-  //       <h2 className="CocktailDb-heading">Bar TheCocktailDB</h2>
-  //       <form onSubmit={handleSearch}>
-  //         <input
-  //           type="search"
-  //           placeholder="Search for a Drink or Spirit"
-  //           onChange={cocktailSearch}
-  //           className="CocktailDb-searchbar"
-  //         ></input>
-  //         <button
-  //           type="button"
-  //           onClick={handleSearch}
-  //           className="CocktailDb-button"
-  //         >
-  //           <img src={searchIcon} alt="search" width="25em" />
-  //         </button>
-  //       </form>
-  //       <CocktailDbResults results={results} />
-  //     </div>
-  //   );
-  // } else {
-  //   initialLoad();
-  //   return "Loading";
-  // }
+  return (
+    <div className="CocktailDb">
+      <h2 className="CocktailDb-heading">Bar TheCocktailDB</h2>
+      <form onSubmit={handleSearch}>
+        <input
+          type="search"
+          placeholder="Search for a Drink or Spirit"
+          onChange={cocktailSearch}
+          className="CocktailDb-searchbar"
+        ></input>
+        <button
+          type="button"
+          onClick={handleSearch}
+          className="CocktailDb-button"
+        >
+          <img src={searchIcon} alt="search" width="25em" />
+        </button>
+      </form>
+      <CocktailDbResults results={results} />
+    </div>
+  );
 };
 
 export default CocktailDb;
