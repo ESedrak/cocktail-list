44258e8f7287352deff435fb08dadecce9f98fc0
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-21 13:53:33 +1000

    add comments for react strict mode

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // This structure of the App contains my own personal cocktail list (through Component Liz)
 // and an online cocktail list https://www.thecocktaildb.com/api.php (through Component  CocktailDb and Random)
 root.render(
+  // React.StrictMode - Cause of network call being fired twice - Strict mode check for development only - does not impact production build
   // <React.StrictMode>
   <App />
   // </React.StrictMode>
