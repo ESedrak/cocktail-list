2b5329fd2b2be99849a52a2957b75e499af3f945
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-10 07:47:04 +1000

    add styling to buttons/form for bar of You

--- src/components/barOfYou/ModalForm/DisplayYourCocktail.js
+++ src/components/barOfYou/ModalForm/DisplayYourCocktail.js
@@ -2,24 +2,25 @@ import "../../../Cocktail.css";
 
 function DisplayYourCocktail({ cocktailInfo, allIngredients, deleteCocktail }) {
 	return (
-		<section className="Cocktail-you-one">
+		<section className="DisplayYourCocktail">
 			<hr />
 			{cocktailInfo.map((details, index) => (
-				<div key={index}>
-					<h3 className="Cocktail-drink-name">{details.drinkName}</h3>
-					<div className="Cocktail-process">
-						<ul className="Cocktail-ingredients" role="presentation">
-							{allIngredients.length > 0 &&
-								allIngredients[index]?.map((showIngredient, index) => (
-									<li key={`${showIngredient} - ${index}`}>
-										{showIngredient.amount} - {showIngredient.ingredient}
-									</li>
-								))}
-						</ul>
-						<p className="Cocktail-instructions">{details.process}</p>
+				<div className="DisplayOneCocktail">
+					<div key={index}>
+						<h3 className="Cocktail-drink-name">{details.drinkName}</h3>
+						<div className="Cocktail-process">
+							<ul className="Cocktail-ingredients" role="presentation">
+								{allIngredients.length > 0 &&
+									allIngredients[index]?.map((showIngredient, index) => (
+										<li key={`${showIngredient} - ${index}`}>
+											{showIngredient.amount} - {showIngredient.ingredient}
+										</li>
+									))}
+							</ul>
+							<p className="Cocktail-instructions">{details.process}</p>
+						</div>
 						<button onClick={() => deleteCocktail(index)}>Delete</button>
 					</div>
-					<hr />
 				</div>
 			))}
 		</section>
