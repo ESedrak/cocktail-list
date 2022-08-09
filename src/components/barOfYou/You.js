79483285dac6cdf1aac11dd80aa67d0e85d7d192
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-10 08:39:07 +1000

    local storage seems to be working

--- src/components/barOfYou/You.js
+++ src/components/barOfYou/You.js
@@ -2,14 +2,26 @@ import Header from "../Header/Header";
 import headerDetails from "../../library/headerDetails";
 import ModalForm from "./ModalForm/ModalForm";
 import DisplayYourCocktail from "../displayCocktails/DisplayYourCocktail";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./You.css";
 
 function You() {
+	// Local storage for each cocktail info and cocktail ingredients
+	const cacheCocktails = localStorage.getItem("stored-cocktails");
+	const cacheIngredients = localStorage.getItem("stored-ingredients");
+	const starterCocktail = cacheCocktails ? JSON.parse(cacheCocktails) : [];
+	const starterIngredients = cacheIngredients
+		? JSON.parse(cacheIngredients)
+		: [];
 	// Modal Form only pops up when Add Cocktail button clicked - otherwise set to false
 	const [modalForm, setModalForm] = useState(false);
-	const [allIngredients, setAllIngredients] = useState([]);
-	const [cocktailInfo, setCocktailInfo] = useState([]);
+	const [cocktailInfo, setCocktailInfo] = useState(starterCocktail);
+	const [allIngredients, setAllIngredients] = useState(starterIngredients);
+
+	useEffect(() => {
+		localStorage.setItem("stored-cocktails", JSON.stringify(cocktailInfo));
+		localStorage.setItem("stored-ingredients", JSON.stringify(allIngredients));
+	}, [cocktailInfo]);
 
 	// createCocktail receives the cocktail data when modal form submited
 	const createCocktail = (drinkInfo, addIngredients) => {
