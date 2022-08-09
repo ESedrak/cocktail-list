9d6e18a9aa531f414eb45d5c6155490dc7487eb8
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-09 14:02:51 +1000

    remove tests code + change layout

--- src/components/barOfYou/ModalForm/DisplayYourCocktail.js
+++ src/components/barOfYou/ModalForm/DisplayYourCocktail.js
@@ -1,78 +1,26 @@
 import "./ModalForm.css";
 
 function DisplayYourCocktail({ cocktailInfo, addIngredient }) {
-	let results = [cocktailInfo, addIngredient];
-	// console.log(cocktailInfo);
-	console.log(addIngredient);
-	// console.log(results);
-
 	return (
 		<div className="DisplayYourCocktail">
-			{/* {results.map((cocktail, index) => ({ cocktail }))} */}
 			<hr />
 			{cocktailInfo.map((details, index) => (
 				<div key={index}>
 					<p>{details.drinkName}</p>
+					<ul role="presentation">
+						{addIngredient.length > 0 &&
+							addIngredient[index].map((showIngredient) => (
+								<li>
+									{showIngredient.ingredient} {showIngredient.amount}
+								</li>
+							))}
+					</ul>
 					<p>{details.process}</p>
-					{addIngredient.length > 0 &&
-						addIngredient[index].map((showIngredient, index) => (
-							<div>
-								{showIngredient.ingredient}
-								{showIngredient.amount}
-							</div>
-						))}
 					<hr />
-					{/* <ul>
-						{addIngredient.map((showIngredient, index) => (
-							{showIngredient.map((oneIngredient, index)=> (
-                                <li>{oneIngredient.ingredient}</li>
-                            ))}
-						))}
-					</ul> */}
 				</div>
 			))}
-			{/* /* {addIngredient.length > 0 &&
-				addIngredient.map(
-					(showIngredient, index) =>
-						showIngredient.map((oneIngredient) => oneIngredient.ingredient)
-
-					// <ul>
-					// 	<li>
-					// 		{showIngredient.ingredient} {showIngredient.amount}
-					// 	</li>
-					// </ul>
-
-					// showIngredient.length > 0 &&
-					// 	showIngredient.map((oneIngredient) => (
-					// 		<ul>
-					// 			<li>
-					// 				{oneIngredient.ingredient} {oneIngredient.amount}
-					// 			</li>
-					// 		</ul>
-					// 	));
-				)}  */}
-			<hr />
-			<br />
 		</div>
 	);
 }
 
 export default DisplayYourCocktail;
-
-// showIngredient.map((oneIngredient, index) => (
-//     <ul key={index}>
-//         {oneIngredient && (
-//             <li>
-//                 {oneIngredient.ingredient} {oneIngredient.amount}
-//             </li>
-//         )}
-//     </ul>
-// ))
-
-// <ul key={index}>
-//     {showIngredient && (
-//         <li>
-//             {showIngredient.ingredient} {showIngredient.amount}
-//         </li>
-//     )}
-// </ul>
