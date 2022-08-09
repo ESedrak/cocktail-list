40ee80d3d900c64023f2e70619444735e9af0b1b
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-09 14:31:32 +1000

    Add styling to you bar to  be consistent with rest of bars

--- src/components/barOfYou/ModalForm/DisplayYourCocktail.js
+++ src/components/barOfYou/ModalForm/DisplayYourCocktail.js
@@ -1,25 +1,26 @@
-import "./ModalForm.css";
+import "../../../Cocktail.css";
 
 function DisplayYourCocktail({ cocktailInfo, addIngredient }) {
 	return (
-		<div className="DisplayYourCocktail">
-			<hr />
+		<section className="Cocktail-you-one">
 			{cocktailInfo.map((details, index) => (
 				<div key={index}>
-					<p>{details.drinkName}</p>
-					<ul role="presentation">
-						{addIngredient.length > 0 &&
-							addIngredient[index].map((showIngredient) => (
-								<li>
-									{showIngredient.ingredient} {showIngredient.amount}
-								</li>
-							))}
-					</ul>
-					<p>{details.process}</p>
+					<h3 className="Cocktail-drink-name">{details.drinkName}</h3>
+					<div className="Cocktail-process">
+						<ul className="Cocktail-ingredients" role="presentation">
+							{addIngredient.length > 0 &&
+								addIngredient[index].map((showIngredient) => (
+									<li>
+										{showIngredient.amount} {showIngredient.ingredient}
+									</li>
+								))}
+						</ul>
+						<p className="Cocktail-instructions">{details.process}</p>
+					</div>
 					<hr />
 				</div>
 			))}
-		</div>
+		</section>
 	);
 }
 
