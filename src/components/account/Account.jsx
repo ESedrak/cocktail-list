9d75bb1a40e88c0c59b016634aa7195be3888717
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-16 13:11:47 +1000

    basic structure of account folder done

---
+++ src/components/account/Account.jsx
@@ -0,0 +1,9 @@
+function Account() {
+	return (
+		<div>
+			<h3>Hello from Account</h3>
+		</div>
+	);
+}
+
+export default Account;
---
+++ src/components/account/Signin.jsx
@@ -0,0 +1,9 @@
+function Signin() {
+	return (
+		<div>
+			<h3>Hello from Signin</h3>
+		</div>
+	);
+}
+
+export default Signin;
