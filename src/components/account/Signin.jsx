8cc163d7a6ccaa7d28ba66d333da55c5d78653bf
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-20 19:46:00 +1000

    basic structure of form complete

--- src/components/account/Signin.jsx
+++ src/components/account/Signin.jsx
@@ -1,8 +1,13 @@
+import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import headerDetails from "../../library/headerDetails";
 import Header from "../Header/Header";
 
 function Signin() {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+
 	return (
 		<div className="Signin">
 			<Header header={headerDetails.account.signin} />
@@ -10,11 +15,15 @@ function Signin() {
 			<form>
 				<div>
 					<label>Email Address</label>
-					<input type="email" />
+					<input type="email" ref={emailRef} />
 				</div>
 				<div>
 					<label>Password</label>
-					<input type="password" />
+					<input type="password" ref={passwordRef} />
+				</div>
+				<div>
+					<label>Confirm Password</label>
+					<input type="password" ref={passwordConfirmRef} />
 				</div>
 			</form>
 		</div>
--- src/components/account/Signup.jsx
+++ src/components/account/Signup.jsx
@@ -1,10 +1,32 @@
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import headerDetails from "../../library/headerDetails";
 import Header from "../Header/Header";
 
 function Signup() {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+
 	return (
 		<div className="Signup">
 			<Header header={headerDetails.account.signup} />
+			<Link to="./Signin"> Sign in</Link>
+			<form>
+				<div>
+					<label>Email Address</label>
+					<input type="email" ref={emailRef} />
+				</div>
+				<div>
+					<label>Password</label>
+					<input type="password" ref={passwordRef} />
+				</div>
+				<div>
+					<label>Confirm Password</label>
+					<input type="password" ref={passwordConfirmRef} />
+				</div>
+				<button>Sign up!</button>
+			</form>
 		</div>
 	);
 }
