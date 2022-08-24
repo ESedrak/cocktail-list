442a11f19b66358a944a911715a273474dfee5fa
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-24 21:12:51 +1000

    finish update profile

--- src/components/account/UpdateProfile.jsx
+++ src/components/account/UpdateProfile.jsx
@@ -8,30 +8,41 @@ function UpdateProfile() {
 	const emailRef = useRef();
 	const passwordRef = useRef();
 	const passwordConfirmRef = useRef();
-	const { signup } = useAuth();
+	const { currentUser, updatePassword, updateEmail } = useAuth();
 	const [error, setError] = useState("");
 	const [loading, setLoading] = useState();
 
 	const navigate = useNavigate();
 
-	async function handleSubmit(e) {
+	function handleSubmit(e) {
 		e.preventDefault();
-
-		signup(emailRef.current.value, passwordRef.current.value);
 
 		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
 			return setError("Passwords do not match!");
 		}
 
-		try {
-			setError("");
-			setLoading(true);
-			// await signup(emailRef.current.value, passwordRef.current.value);
-			navigate("/dashboard");
-		} catch (error) {
-			console.log(error);
-			setError("Failed to create an account");
+		const promises = [];
+		setLoading(true);
+		setError("");
+
+		if (emailRef.current.value !== currentUser.email) {
+			promises.push(updateEmail(emailRef.current.value));
 		}
+
+		if (passwordRef.current.value) {
+			promises.push(updatePassword(passwordRef.current.value));
+		}
+
+		Promise.all(promises)
+			.then(() => {
+				navigate("/dashboard");
+			})
+			.catch(() => {
+				setError("Failed to update account");
+			})
+			.finally(() => {
+				setLoading(false);
+			});
 
 		setLoading(false);
 	}
