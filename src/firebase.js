3c3742835677987994dc0ef98ab01aaf83aaccdc
ESedrak <elizabeth_sedrak@hotmail.com>
2022-08-28 09:12:57 +1000

    change names to match what is needed

--- src/firebase.js
+++ src/firebase.js
@@ -4,8 +4,8 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
